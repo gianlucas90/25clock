69110ac8fe1f9c1eb0237ab127987efdaec45dc4
Gianluca <gianlucazin1990@gmail.com>
2020-10-02 10:14:41 +0200

    Updates

---
+++ precache-manifest.63053ce035e6c3a9dfb5beaca7288d01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc1508880f42fabf0d3aa99e0bf6abf3",
+    "url": "/25clock/index.html"
+  },
+  {
+    "revision": "ecfd69efc7cb932eda33",
+    "url": "/25clock/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "f7470cb79b57b4c29161",
+    "url": "/25clock/static/js/2.beeddf04.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/25clock/static/js/2.beeddf04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecfd69efc7cb932eda33",
+    "url": "/25clock/static/js/main.a7dda5d1.chunk.js"
+  },
+  {
+    "revision": "29380f82612fa56a8125",
+    "url": "/25clock/static/js/runtime-main.5afed8c1.js"
+  }
+]);
